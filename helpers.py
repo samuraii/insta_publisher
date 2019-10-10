e551b2dd159e59e74f33708309a028ff2e70fece
samuraii <7thsamurai@mail.ru>
2019-10-10 13:47:03 +0300

    Использовал makedirs с нужным параметром для создания папки

--- helpers.py
+++ helpers.py
@@ -3,14 +3,10 @@ import requests
 from PIL import Image
 
 
-def create_folder(directory):
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
-
 def download_image(image_url, image_name):
     response = requests.get(image_url)
     create_folder('images')
+    os.makedirs('images', exist_ok=True)
     with open(f'images/{image_name}', 'wb') as file:
         file.write(response.content)
 
